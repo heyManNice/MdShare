8623c69485eb9b0c85c65c1562604906a75a9696
heyManNice <3031978990@qq.com>
2024-01-14 16:54:54 +0800

    添加img请求方案

--- main.js
+++ main.js
@@ -6,19 +6,16 @@ const server = require('express')();
 const rateLimit= require('express-rate-limit').rateLimit;
 const port = 8081;
 
-server.use(rateLimit({
-	windowMs:60 * 1000,
-	limit: 60,
-	standardHeaders: 'draft-7',
-	legacyHeaders: false,
-}))
+//访问限制
+server.use(rateLimit({windowMs:60 * 1000, limit: 60, standardHeaders: 'draft-7', legacyHeaders: false,}));
 
 //加载服务组件
 loadServer('pageRoute');
 
 //路由
 server.get('/', pageRoute.index )
- 
+server.get('/img/:filename', pageRoute.img )
+
 server.listen(port, () => {
     print(`系统已运行在 ${port}`);
 })
--- server/pageRoute.js
+++ server/pageRoute.js
@@ -1,7 +1,16 @@
- const path = require('path');
+const path = require('path');
+const fs = require("fs");
 
 pageRoute = {
     index:async function(req, res){
-        res.sendFile(path.join(main_dirname,"public","index.html"));
+        return res.sendFile(path.join(main_dirname,"public","index.html"));
+    },
+    img:async function(req, res){
+        let filepath = path.join(main_dirname,"public","img",req.params.filename)
+        if(!fs.existsSync(filepath)){
+            res.status(404)
+            return res.send({code:404});
+        }
+        return res.sendFile(filepath);
     }
 }
