78d0339431f64b5802327c05a6d0c1b8eefdc9e9
heyManNice <3031978990@qq.com>
2024-01-25 11:05:27 +0800

    为admin启用日志开关

--- server/pageRoute.js
+++ server/pageRoute.js
@@ -61,17 +61,21 @@ pageRoute = {
         return res.sendFile(path.join(main_dirname,"public","admin","admin_login.html"));
     },
     admin_post:async function(req,res){
-        if(req.body.account!="admin" || req.body.password!="12345"){
-            clearTimeout(sweet.timer);
-            let log_text = `第${sweet.login_try}次尝试登陆并且失败`
-            sweet.timer=setTimeout(()=>{
-                sweet.log(req,log_text);
-            },10000);
+        if(req.body.account!="admin" || req.body.password!=config.sweet.password){
+            if(config.sweet.log){
+                clearTimeout(sweet.timer);
+                let log_text = `第${sweet.login_try}次尝试登陆并且失败`
+                sweet.timer=setTimeout(()=>{
+                    sweet.log(req,log_text);
+                },10000);
+            }
             sweet.login_try++;
             return res.sendFile(path.join(main_dirname,"public","admin","admin_fail.html"));
         }
-        clearTimeout(sweet.timer);
-        sweet.log(req,`第${sweet.login_try}次尝试登陆并且成功`);
+        if(config.sweet.log){
+            clearTimeout(sweet.timer);
+            sweet.log(req,`第${sweet.login_try}次尝试登陆并且成功`);
+        }
         let text = `恭喜你！经过${sweet.login_try}次尝试，攻破了本站的管理员弱密码`+config.sweet.response;
         sweet.login_try = 1;
         return res.send(text);
